4afe37ec8ba754690803c8efb4ac28f35277023c
unknown <Jpototsk@FSO0127.FSO.Arizona.EDU>
2016-03-16 17:03:41 -0700

    Stuff I did....cause I did....when I did....and then some stuff.

---
+++ Week 1/CSE110-LAB1-Pototsky/src/Lab1.java
@@ -0,0 +1,23 @@
+//***********************************************************
+//Answers to the lab questions
+//1. This is my first program!
+//2. No question asked
+//3. Hello, my name is Jason PototskyThis is my first program!
+//4. No question asked
+//5. No question asked
+//***********************************************************
+//***********************************************************
+// Name: Jason Pototsky
+// Title: Lab1.java
+// Description: Prints out My first program and my name.
+// Time spent: 5 minutes
+// Date: 3/16/2016
+//***********************************************************
+public class Lab1
+{
+public static void main (String[] args)
+{
+	System.out.println("Hello, my name is Jason Pototsky");
+	System.out.println("This is my first program!");
+}
+}
